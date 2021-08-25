d9ad083cbdcca0a7efe7ebdc35f21b6c58e33cb1
Lucas Cota <lucascdornelas@gmail.com>
2021-08-25 16:36:31 -0300

    add adjacente

--- adjacente.pl
+++ adjacente.pl
@@ -1,7 +1,7 @@
 igual([], []).
-igual([A|B], [A|C]) :- igual(B, C).
+igual([E|B], [E|C]) :- igual(B, C).
 
-adjacente(X, Y, [A,B|R]) :- igual([X], [A]), igual([Y], [B]);
-adjacente(X, Y, [B|R]).
+adjacente(X, Y, [A,B|V]) :- igual([X], [A]), igual([Y], [B]);
+adjacente(X, Y, [B|V]).
 
 /* Verificar se	um elemento	X é	adjacente a	um elemento	Y. */
