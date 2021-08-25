ec0446c0a108435fd46dc5b18f07ecb239f2ea1d
ErickHDdS <erickhenriquedds@gmail.com>
2021-08-25 11:42:05 -0300

    FIX: metodo medio imprimindo 'floats'

--- medio.pl
+++ medio.pl
@@ -1,10 +1,10 @@
-soma([], 0).
+soma([], 0.0).
 soma([E|V], S) :- soma(V, S1), S is E+S1.
 
 nelementos([], 0).
 nelementos([_|V], N) :- nelementos(V, C), N is C+1.
 
-medio([], 0).
-medio(V, M) :- soma(V, S), nelementos(V, N), M is float(S/N).
+medio([], 0.0).
+medio(V, M) :- soma(V, S), nelementos(V, N), M is S/N.
 
 /* Obter o valor médio de uma lista de inteiros. */
