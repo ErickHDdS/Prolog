01689f5add59064704ef7bfa18fd739528c8c067
Lucas Cota <lucascdornelas@gmail.com>
2021-08-25 16:51:30 -0300

    feat: add rotacionarn

--- rotacionarn.pl
+++ rotacionarn.pl
@@ -1,11 +1,11 @@
-concatenar([], L, L).
-concatenar([X|L1], L2, [X|L3]) :- concatenar(L1, L2, L3).
+concatenar([], V, V).
+concatenar([V|V1], V2, [V|V3]) :- concatenar(V1, V2, V3).
 
 rotacionar([], []).
-rotacionar([A|B], R) :- concatenar(B, [A], R).
+rotacionar([E1|E2], R) :- concatenar(E2, [E1], R).
 
-rotacionarn(0, A, A).
-rotacionarn(1, A, B) :- rotacionar(A, B).
-rotacionarn(R, A, B) :- rotacionarn(X, A, C), rotacionar(C, B), R is X+1.
+rotacionarn(0, E1, E1).
+rotacionarn(1, E1, E2) :- rotacionar(E1, E2).
+rotacionarn(R, E1, E2) :- rotacionarn(X, E1, E3), rotacionar(E3, E2), R is X+1.
 
 /* Rotacionar uma lista	'n' posições. */
